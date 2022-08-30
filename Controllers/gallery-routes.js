bab5f93e29106d8adc8c284a8c61ff2b9e2fa992
Booker King <btking1@my.waketech.edu>
2022-08-30 19:22:31 -0400

    .

--- Controllers/gallery-routes.js
+++ Controllers/gallery-routes.js
@@ -1,20 +1,7 @@
 const router = require("express").Router();
 // const sequelize = require('../config/connection');
 const cloudinary = require("../config/cloud-connection");
-// const { Post, User, Comment, Vote } = require('../models');
 
-// router.get("/", (req, res) => {
-
-//   // get all images from cloudinary
-
-//   cloudinary.api.root_folders(function (error, result) {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       res.render("gallery", { images: result.folders.prompts});
-//     }
-//   });
-// });
 
 router.get("/", (req, res) => {
   // show images from cloudinary in the folder prompts
