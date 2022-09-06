3c04a356ebe2cb6456a699fb713733c458980d86
Booker King <btking1@my.waketech.edu>
2022-09-06 18:19:11 -0400

    .

--- Controllers/home-routes.js
+++ Controllers/home-routes.js
@@ -28,13 +28,15 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
+    .then(dbPostData => 
+      {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
       res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn
-      });
+        // posts,
+        // loggedIn: req.session.loggedIn
+      }
+      );
     })
     .catch(err => {
       console.log(err);
