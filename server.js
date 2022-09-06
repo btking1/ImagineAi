abec9564252e954408e878db3c2e4bec84b93768
Booker King <btking1@my.waketech.edu>
2022-09-06 19:40:18 -0400

    fix handlebars

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ app.use(session(sess));
 const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
-app.set('views', path.join(__dirname, 'Views'));
+app.set(
+  "views engine","handlebars",
+  path.join(__dirname,"Views")
+);
+// app.set('views', path.join(__dirname, 'Views'));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
