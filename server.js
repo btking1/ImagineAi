9467c8b463def5407cb8cb11607319f2dd473554
Booker King <btking1@my.waketech.edu>
2022-09-06 21:36:25 -0400

    .

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session(sess));
 const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
-app.set("view engine");
+app.set("views", "views engine handlebars");
 // app.set('views', path.join(__dirname, 'Views'));
 
 app.use(express.json());
